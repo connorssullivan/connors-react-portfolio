e1d9662a407d802ca2a22661a464e49d1f36a592
connorssullivan <connorssullivan25@gmail.com>
2023-12-23 16:06:56 -0500

    pushing

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav.js"; 
+import About from "./about/about.js";
+import Skills from "./skills/skills.js";
+import Projects from "./projects/projects.js";
+import Contact from "./cotact/contact.js";
+
+const App = () => {
+  return (
+  <>
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={About}/>
+        <Route path="/skills" element={Skills}/>
+        <Route path="/projects" element={Projects}/>
+        <Route path="/contact" element={Contact}/>
+      </Routes>
+    </Router>
+  </>
+  );
+};
+
+export default App;
---
+++ src/skills/skills.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class skills extends Component {
+  render() {
+    return (
+      <div>Skills</div>
+    )
+  }
+}
